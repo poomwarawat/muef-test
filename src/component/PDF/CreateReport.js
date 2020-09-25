4ffdff835e85829d262ef1c74c70f4b1918298d5
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-09-25 14:07:44 +0700

    edit pdf report to csv report

--- src/component/PDF/CreateReport.js
+++ src/component/PDF/CreateReport.js
@@ -27,6 +27,15 @@ let initState = {
   FI: "",
   EMI: "",
   GEC: "",
+  rawINH: "",
+  rawSHF: "",
+  rawEC: "",
+  rawWM: "",
+  rawPO: "",
+  rawISCI: "",
+  rawFI: "",
+  rawEMI: "",
+  rawGEC: "",
 };
 
 const CreateReport = () => {
@@ -37,6 +46,7 @@ const CreateReport = () => {
   const fetchReport = async () => {
     const fetch = await API.get("/admin/report");
     const res = await fetch.data;
+    console.log(res);
     const data = res.data;
     const score = res.allScore;
     const {
@@ -51,6 +61,11 @@ const CreateReport = () => {
       T_SCORE_GEC,
     } = score;
     const {
+      EC,
+      INH,
+      PO,
+      SHF,
+      WM,
       fname,
       lname,
       nname,
@@ -80,7 +95,20 @@ const CreateReport = () => {
       district: district,
       province: province,
       create_time: new Date(create_time),
-      user: user,
+      rawEC: EC,
+      rawINH: INH,
+      rawSHF: SHF,
+      rawPO: PO,
+      rawWM: WM,
+      rawISCI: parseInt(INH) + parseInt(EC),
+      rawFI: parseInt(SHF) + parseInt(EC),
+      rawEMI: parseInt(WM) + parseInt(PO),
+      rawGEC:
+        parseInt(INH) +
+        parseInt(EC) +
+        parseInt(WM) +
+        parseInt(PO) +
+        parseInt(SHF),
       INH: T_SCORE_INH.resultText[1],
       SHF: T_SCORE_SHF.resultText[1],
       EC: T_SCORE_EC.resultText[1],
@@ -90,93 +118,90 @@ const CreateReport = () => {
       FI: T_SCORE_FI.resultText[1],
       EMI: T_SCORE_EMI.resultText[1],
       GEC: T_SCORE_GEC.resultText[1],
+      user: user,
     });
   };
-
-  const pdfGenerator = () => {
-    const content = `<div id="pdf-generator">   
-    <table>
-          <thead id="colortableheade">
-            <tr>
-            <th id="colortable">ชื่อ</th>
-              <th id="colortable">นามสกุล</th>
-              <th id="colortable">ชื่อเล่น</th>
-              <th id="colortable">เพศ</th>
-              <th id="colortable">วันเกิด</th>
-              <th id="colortable">เงินเดือน</th>
-              <th id="colortable">เลขที่</th>
-              <th id="colortable">เลขห้อง</th>
-              <th id="colortable">โรงเรียน</th>
-              <th id="colortable">ตำบล</th>
-              <th id="colortable">อำเภอ</th>
-              <th id="colortable">จังหวัด</th>
-              <th id="colortable">คุณครู</th>
-              <th id="colortable">วันที่ประเมิน</th>
-              <th id="colortable">INH</th>
-              <th id="colortable">SHF</th>
-              <th id="colortable">EC</th>
-              <th id="colortable">WM</th>
-              <th id="colortable">PO</th>
-              <th id="colortable">ISCI</th>
-              <th id="colortable">FI</th>
-              <th id="colortable">EMI</th>
-              <th id="colortable">GEC</th>
-            </tr>
-          </thead>
-          <tbody id="colortable">
-            <tr>
-              <td id="colortable">${data.fname}</td>
-              <td id="colortable">${data.lname}</td>
-              <td id="colortable">${data.nname}</td>
-              <td id="colortable">${data.gender}</td>
-              <td id="colortable">${data.birthday.getDay()}/${
-      parseInt(data.birthday.getMonth()) + 1
-    }/${parseInt(data.birthday.getFullYear()) + 543}</td>
-              <td id="colortable">${data.salary}</td>
-              <td id="colortable">${data.stdCode}</td>
-              <td id="colortable">${data.stdRoom}</td>
-              <td id="colortable">${data.schoolname}</td>
-              <td id="colortable">${data.region}</td>
-              <td id="colortable">${data.district}</td>
-              <td id="colortable">${data.province}</td>
-              <td id="colortable">${data.user}</td>
-              <td id="colortable">${data.create_time.getDay()}/${
-      parseInt(data.create_time.getMonth()) + 1
-    }/${parseInt(data.create_time.getFullYear()) + 543}</td>
-              <td id="colortable">${data.INH}</td>
-              <td id="colortable">${data.SHF}</td>
-              <td id="colortable">${data.EC}</td>
-              <td id="colortable">${data.WM}</td>
-              <td id="colortable">${data.PO}</td>
-              <td id="colortable">${data.ISCI}</td>
-              <td id="colortable">${data.FI}</td>
-              <td id="colortable">${data.EMI}</td>
-              <td id="colortable">${data.GEC}</td>
-            </tr>
-          </tbody>
-        </table> 
-  </div>`;
-
-    const opt = {
-      margin: 0.75,
-      filename: "report.pdf",
-      image: { type: "png", quality: 4 },
-      html2canvas: { scale: 1 },
-      jsPDF: {
-        orientation: "landscape",
-        unit: "mm",
-        format: "a1",
-        putOnlyUsedFonts: true,
-        floatPrecision: 16, // or "smart", default is 16
-      },
-    };
-
-    // Save the PDF
-    html2pdf().set(opt).from(content).save();
+  const csvGenerator = () => {
+    const arrObj = [data];
+    let csv = JSON2CSV(arrObj).toString();
+    // console.log(csv.split('"user"'));
+    const header = csv.split('"user"')[0].toString() + '"user"';
+    csv = csv.split('"user"')[1];
+    const result = header + "\r\n" + csv;
+    console.log(result);
+    const test = DownloadCSV(result);
+    // window.open(`data:text/csv;charset=utf-8,${result}`);
+    // arrObj.reduce((acc, curr) => (`${acc}${Object.values(curr).join(",")}\n`), "")
   };
+  const DownloadCSV = (csvString) => {
+    var blob = new Blob([csvString]);
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, "all_report.csv");
+    } else {
+      var a = window.document.createElement("a");
+
+      a.href = window.URL.createObjectURL(blob, {
+        type: "text/plain",
+      });
+      a.download = "all_report.csv";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+  const JSON2CSV = (objArray) => {
+    var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
+
+    var str = "";
+    var line = "";
+
+    // get all distinct keys
+    let titles = [];
+    for (var i = 0; i < array.length; i++) {
+      let obj = array[i];
+      Object.entries(obj).forEach(([key, value]) => {
+        //console.log('key=', key, "   val=", value );
+        if (titles.includes(key)) {
+          console.log(key, "exists");
+        } else {
+          titles.push(key);
+        }
+      });
+    }
+    let htext = '"' + titles.join('","') + '"';
+    // add to str
+    str += htext + "\r\n";
+    //
+    // lines
+    for (var i = 0; i < array.length; i++) {
+      var line = "";
+      // get values by header order
+      for (var j = 0; j < titles.length; j++) {
+        // match keys with current header
+        let obj = array[i];
+        let keyfound = 0;
+        // each key/value pair
+        Object.entries(obj).forEach(([key, value]) => {
+          if (key == titles[j]) {
+            // console.log('equal tit=', titles[j] , ' e key ', key ); // matched key with header
+            line += ',"' + value + '"';
+            keyfound = 1;
+            return false;
+          }
+        });
+        if (keyfound == 0) {
+          line += ',"' + '"'; // add null value for this key
+        } // end loop of header values
+      }
+
+      str += line.slice(1) + "\r\n";
+    }
+    return str;
+  };
+
   return (
     <div>
-      <Button onClick={pdfGenerator} color="primary">
+      <Button onClick={csvGenerator} color="primary">
         ดาวน์โหลดรายงาน
       </Button>
       {/* {data.fname !== "" && (
